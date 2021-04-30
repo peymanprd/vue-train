5402ff0cf2f4b638527f8a4581354a1f470a96af
Peyman Pirzadeh <dev.pirzadeh@gmail.com>
2021-04-30 20:14:50 +0430

    add start files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container my-4 home">
+        <Todo />
+        <label for="users">select todo id:</label>
+        <input
+            type="number"
+            id="users"
+            name="users"
+            v-model="userID"
+            @input="filter"
+        />
+        <input
+            type="checkbox"
+            name="compelete"
+            id="compelete"
+            v-model="isDone"
+            @change="filter"
+        />
+        <ul>
+            <li v-for="todo in todos">{{ todo.title }}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Todo from '@/components/Todo.vue'
+import axios from 'axios'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    components: { Todo },
+    data: () => ({
+        userID: {
+            type: Number,
+            default: 0,
+        },
+        isDone: {
+            type: Boolean,
+            default: false,
+        },
+        todos: [],
+    }),
+    methods: {
+        filter() {
+            axios
+                .get('https://jsonplaceholder.typicode.com/todos', {
+                    params: { userId: this.userID, completed: this.isDone },
+                })
+                .then(res => {
+                    this.todos = res.data
+                    console.log(this.todos)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
+    },
+    created() {},
 }
 </script>
