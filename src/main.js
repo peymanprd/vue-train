5be329dd8afc5c31a74e20a712d9f5193e815b07
Peyman Pirzadeh <dev.pirzadeh@gmail.com>
2021-05-29 00:22:43 +0430

    add-unic-item

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import '../node_modules/bootstrap/scss/bootstrap.scss'
+import 'bootstrap'
+
+
+const app = createApp(App)
+app.use(store)
+    .use(router)
+    .mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home mt-4">
+        <div class="my-2" v-for="(user, index) in users" :key="user.id">
+            <button @click="addUser(index)" class="btn btn-primary">+</button>
+            {{ user.name + ' : ' + user.count }}
+        </div>
+        <hr class="my-4" />
+        <div v-if="admins">
+            <div v-for="admin in admins" :key="admin.id">
+                {{ admin.name + ' : ' + admin.count }}
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import API_Service from '../services/api'
+import { ref, reactive } from 'vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    setup() {
+        const user = ref({})
+        const users = ref()
+        const admins = ref([])
+
+        function fetchUsers() {
+            API_Service.getUsers()
+                .then(res => {
+                    users.value = res.data
+                })
+                .catch(err => err)
+        }
+
+        function addUser(index) {
+            user.value = users.value[index]
+            console.log(user.value)
+
+            if (!admins.value.length && admins.value.length <= 0) {
+                user.value.count = 1
+                admins.value.push(user.value)
+            } else {
+                let whichUser
+                let existing = admins.value.filter((item, key) => {
+                    if (item.id === user.value.id) {
+                        whichUser = key
+                        return true
+                    } else {
+                        return false
+                    }
+                })
+                if (existing.length) {
+                    admins.value[whichUser].count++
+                } else {
+                    user.value.count = 1
+                    admins.value.push(user.value)
+                }
+            }
+
+            console.log(admins.value)
+        }
+
+        return { user, users, fetchUsers, admins, addUser }
+    },
+    created() {
+        this.fetchUsers()
+    },
 }
 </script>
